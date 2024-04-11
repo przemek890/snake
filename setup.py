0aed4d48e12a31739b1330bea0fac30c17d341ef
przemek890 <przemek.899@wp.pl>
2024-04-11 16:58:59 +0200

    -

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='Snake_module',
     version='0.1',
-    packages=find_packages(),
+    packages=find_packages() + ['tools'],
     py_modules=['run'],
     install_requires=[
         'pytest',
