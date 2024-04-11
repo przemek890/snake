c6fc8ae847e5c010d4e57e896ba9cbb6c80aa497
przemek890 <przemek.899@wp.pl>
2024-04-11 21:12:23 +0200

    -

--- setup.py
+++ setup.py
@@ -4,6 +4,9 @@ setup(
     name='Snake_module',
     version='0.1',
     packages=find_packages(),
+    package_data={
+        'Snake_module': ['tools/*', 'snake/*', 'run.py'],
+    },
     install_requires=[
         'pytest',
         'matplotlib'
