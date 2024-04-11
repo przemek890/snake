92e060b8735269bb0f218b9f56d99415ccb4e62a
przemek890 <przemek.899@wp.pl>
2024-04-12 01:40:00 +0200

    -

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name='Snake_module',
     version='0.1',
+    author='Przemysław Janiszewski',
+    author_email='przemek.899@wp.pl',
     packages=find_packages(),
     install_requires=[
         'pytest',
