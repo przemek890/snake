1d000478c51b92917fc2ea740b92dbc177741aae
przemek890 <przemek.899@wp.pl>
2024-04-11 15:06:34 +0200

    -

--- clear.sh
+++ clear.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 if [ "$(docker ps -a -q)" ]; then
-  docker stop $(docker ps -a -q)
-  docker rm $(docker ps -a -q)
+  docker stop -f $(docker ps -a -q)
+  docker rm -f $(docker ps -a -q)
 fi
 
 if [ "$(docker images -q)" ]; then
-  docker rmi $(docker images -q)
+  docker rmi -f $(docker images -q)
 fi
 
 docker ps -a
